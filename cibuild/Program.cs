98bcd6a20266b7b1cc4af5fd06c4e333daf04f5d
雷鳴 <sorayuki@live.com>
2022-10-13 14:19:55 +0800

    remove x86-32 build

--- cibuild/Program.cs
+++ cibuild/Program.cs
@@ -31,7 +31,7 @@ class Program {
         await DownloadFile($"https://github.com/obsproject/obs-studio/archive/refs/tags/{obsver}.zip", "obs-src.zip");
         ZipFile.ExtractToDirectory($"obs-src.zip", $"obs-src-tmp");
         Directory.Move(Directory.EnumerateDirectories("obs-src-tmp").ToArray()[0], "obs-src");
-        foreach(var arch in new string[] {"x86", "x64"}) {
+        foreach(var arch in new string[] {/*"x86",*/ "x64"}) {
             await DownloadFile($"https://cdn-fastly.obsproject.com/downloads/OBS-Studio-{obsver}-Full-{arch}.zip", $"obs-bin-{arch}.zip");
             Directory.CreateDirectory($"obs-bin-{arch}");
             ZipFile.ExtractToDirectory($"obs-bin-{arch}.zip", $"obs-bin-{arch}");
